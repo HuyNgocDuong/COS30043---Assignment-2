cd53d61f1e3d239e06f5d4c99f5b7696678f72df
HuyNgocDuong <ngochuyduong288@gmail.com>
2025-04-02 01:43:33 +1100

    Setup and testing

---
+++ insight-hire/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container text-center">
+    <h1 class="text-primary">Hello Vue + Bootstrap!</h1>
+    <button class="btn btn-success">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+h1 {
+  margin-top: 20px;
+}
+</style>
